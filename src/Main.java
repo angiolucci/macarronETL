943e27e2ca71017fdf94fc4ffca33342da17fcd2
Vinícius Angiolucci Reis <angiolucci@gmail.com>
2014-10-23 19:58:36 -0200

    alteração no tratamento de exception mais externa

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
 	public static void main(String[] args){
 		try {
 			System.out.println("\nPopulate DB using:\n" + args[0] + "\n");
-			new DBPopulator(args[0]);
 		} catch (IndexOutOfBoundsException e) {
 			System.err.println("Você deve especificar um arquivo de origem!");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 			System.exit(-1);
 		}
+		
+		new DBPopulator(args[0]);
 	}
 }
